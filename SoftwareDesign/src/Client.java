34ae7b89aaf829de485cbdf57519046b34becf9b
FliesWithWind <w.zydecki@gmail.com>
2014-11-10 11:18:18 +0900

    Added simple clientside text menu.

--- SoftwareDesign/src/Client.java
+++ SoftwareDesign/src/Client.java
@@ -3,6 +3,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;	
 import java.net.SocketException;
+import java.util.Scanner;
 
 public class Client {
 	private Socket socket = null;
@@ -24,16 +25,53 @@ public class Client {
     			Room testRoom = new Room("TestRoom","TestLocation",10);
     			System.out.println("Object to be sent: " + testRoom);
     			outputStream.writeObject(testRoom);
+    			socket.close();
     		} catch (SocketException se){
     			se.printStackTrace();
     		} catch (IOException e){
     			e.printStackTrace();
     		}
     	}
+    	isConnected = false;
+    }
+    
+    public void printMenu(){
+    	System.out.println("***********Client console***********");
+    	System.out.println("1. Run connection and transfer test");
+    	System.out.println("2. Create a room");
+    	System.out.println("3. List stored rooms");
+    	System.out.println("4. Send a room to server");
+    	System.out.println("0. Exit");
     }
     
     public static void main(String[] args) {
     	Client client = new Client();
-    	client.transferTest();
+    	Scanner in = new Scanner(System.in);
+    	boolean exit = false;
+    	int menuItem;
+    	do {
+    		client.printMenu();
+    		menuItem = in.nextInt();
+    		switch (menuItem){
+    			case 1:
+    				client.transferTest();
+    				break;
+    			case 2:
+    				System.out.println("Not implemented yet");
+    				break;
+    			case 3:
+    				System.out.println("Not implemented yet");
+    				break;
+    			case 4:
+    				System.out.println("Not implemented yet");
+    				break;
+    			case 0:
+    				exit = true;
+    				in.close();
+    				break;
+    			default:
+    				System.out.println("Invalid choice");
+    		}
+    	} while (!exit);
     }
 }
