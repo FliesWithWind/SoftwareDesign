d0d641be74dbbdf9674c827625388477814ee2eb
SON <zkalffntm@gmail.com>
2014-11-16 18:27:50 +0900

    server network

---
+++ MRRserver/src/network/Listener.java
@@ -0,0 +1,74 @@
+package network;
+
+import java.io.IOException;
+import java.net.*;
+
+public class Listener implements Runnable
+{
+	private static final int PORT = 44444;
+
+	private ServerSocket		ss;
+	private control.Processor	processor;
+	
+	
+	public Listener(control.Processor processor)
+	{
+		this.processor = processor;
+	}
+	
+	// thread performs
+	public void run()
+	{
+		// create listening socket
+		System.out.println("Creating Listening Socket...");
+		try
+		{
+			ss = new ServerSocket(PORT);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			return;
+		}
+
+		// listen client
+		System.out.println("Listening...");
+		while(true)
+		{
+			try
+			{
+				Socket cs = ss.accept();
+				if(cs == null)
+				{
+					System.out.println("");
+					break;
+				}
+				
+				System.out.println("Client " + 
+									cs.getInetAddress().getHostName() + "(" + 
+									cs.getInetAddress().getHostAddress() + ")" +
+									" connected!");
+				new Thread(new Network(processor, cs)).start();
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+				return;
+			}
+		}
+			
+	}
+	
+	// close server
+	public void closeSocket()
+	{
+		try
+		{
+			ss.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
--- MRRserver/src/network/Network.java
+++ MRRserver/src/network/Network.java
@@ -1,6 +1,77 @@
 package network;
 
-public class Network 
+import java.net.*;
+import java.io.*;
+
+public class Network implements Runnable
 {
+	private control.Processor	processor;
+	private Socket				client;
+
+	private ObjectInputStream	is;
+	private ObjectOutputStream	os;
+	
+	// generates Input, Output Stream
+	public Network(control.Processor processor, Socket client)
+	{
+		this.processor	= processor;
+		this.client		= client;
+		try
+		{
+			is = new ObjectInputStream(client.getInputStream());
+			os = new ObjectOutputStream(client.getOutputStream());
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	// thread performs
+	public void run()
+	{
+		if(is == null || os == null) System.out.println("Stream Error");
+		else while(true)
+		{
+			try
+			{
+				Packet packet = (Packet) is.readObject();	// read from client
+				if(packet == null) break; // if client sends EOF, break the loop
+				// process packet in processor and response to client
+				os.writeObject(processor.process(packet));
+			}
+			catch(ClassNotFoundException e)
+			{
+				System.out.println("Invalid packet form");
+				e.printStackTrace();
+				break;
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+				break;
+			}
+		}
+
+		// client disconnected : notify and close streams
+		try
+		{
+			System.out.println("Client " +
+					client.getInetAddress().getHostName() + "(" + 
+					client.getInetAddress().getHostAddress() + ")" +
+					" disconnected!");
+			
+			is.close();
+			os.close();
+			client.close();
+			
+			System.out.println("Streams closed succesfully.");
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
 	
 }
