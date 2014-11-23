f11facd20fcf39bbe77c4886fa7214c2f07d885f
FliesWithWind <w.zydecki@gmail.com>
2014-11-23 16:15:30 +0900

    Added possibility to add and list rooms.

--- MRRserver/src/console/ServerConsole.java
+++ MRRserver/src/console/ServerConsole.java
@@ -4,32 +4,59 @@ import java.io.*;
 import java.util.ArrayList;
 
 import datatype.Account;
+import datatype.Room;
 import network.*;
 
 public class ServerConsole {	
 	
 	private static ArrayList<Account> AccountList;
+	private static ArrayList<Room> RoomList;
 	
 	public ServerConsole(){
 		
 	}
 	
+	/**
+	 * Method to find and return Account object using id
+	 * Returns null if Account was not found.
+	 * @param id
+	 * @return
+	 */
+	
+	private static Account findAccount(String id){
+		Account tmp;
+		for(int i=0;i<AccountList.size();i++){
+			if(AccountList.get(i).getId().equals(id)){
+				tmp=AccountList.get(i);
+				return tmp;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Adding new account to AccounList
+	 * @param command
+	 * @return
+	 */
+	
 	private static boolean newAccount(String command){
-    	String[] cmd = command.split(" ");
-    	if(cmd.length!=8){
+    	String[] cmd = command.split(" ");	//Split string into parts
+    	if(cmd.length!=8){					//Check if command is correct
     		System.out.println("[Error] Invalid number of parameters!");
     		return false;
     	} else{
-    		int type=0;
+    		int type=0; // get account type value
     		if(cmd[3].equalsIgnoreCase("user"))
     			type=1;
     		else if(cmd[3].equalsIgnoreCase("staff"))
     			type=2;
     		else if(cmd[3].equalsIgnoreCase("manager"))
     			type=3;
+    		// Debug to check if string is split correctly
+    		//for(int i=0;i<cmd.length;i++)
+    		//	System.out.println(i + " " + cmd[i]);
     		
-    		for(int i=0;i<cmd.length;i++)
-    			System.out.println(i + " " + cmd[i]);
     		Account tmp = new Account(cmd[1],cmd[2],type,cmd[4],cmd[5],cmd[6],cmd[7]);
     		try {
     			AccountList.add(tmp);
@@ -42,13 +69,61 @@ public class ServerConsole {
     	}
 	}
 	
-	private static void list(){
+	/**
+	 * Add new room to RoomList
+	 */
+	
+	private static boolean newRoom(String command){
+    	String[] cmd = command.split(" ");	//Split string into parts
+    	if(cmd.length!=7){					//Check if command is correct
+    		System.out.println("[Error] Invalid number of parameters!");
+    		return false;
+    	} else{
+    		// Debug to check if string is splited correctly
+    		//for(int i=0;i<cmd.length;i++)
+    		//	System.out.println(i + " " + cmd[i]);
+    		
+    		int city=0; //get city value
+    		if(cmd[3].equalsIgnoreCase("seoul"))
+    			city=1;
+    		else if(cmd[3].equalsIgnoreCase("daejeon"))
+    			city=2;
+    		else if(cmd[3].equalsIgnoreCase("daegu"))
+    			city=3;
+    		else if(cmd[3].equalsIgnoreCase("jeonju"))
+    			city=4;
+  
+    		Account acc = findAccount(cmd[1]);
+    		if(acc == null){
+    			System.out.println("Account id does not exist.");
+    			return false;
+    		} else {
+	    		Room tmp = new Room(acc,cmd[2],city,cmd[4],Integer.parseInt(cmd[5]),Float.parseFloat(cmd[6]));
+	    		try {
+	    			RoomList.add(tmp);
+	    		}
+	    		catch (Exception ex){
+	    			System.out.println("[Error] Problem occured while adding to the list.");
+	    		}
+	    		System.out.println("New account sucessfully created.");
+	    		return true;
+    		}
+    	}
+	}
+	
+	private static void listAccounts(){
 		for(int i=0;i<AccountList.size();i++)
 			System.out.println(AccountList.get(i).getName() + " " + AccountList.get(i).getId() + " " + AccountList.get(i).getPhonenum());
 	}
 	
+	private static void listRooms(){
+		for(int i=0;i<RoomList.size();i++)
+			System.out.println(RoomList.get(i).getName() + " " + RoomList.get(i).getCity() + " " + RoomList.get(i).getMaxcapacity());
+	}
+	
 	public static void main(String[] args){
 		AccountList = new ArrayList<Account>();
+		RoomList = new ArrayList<Room>();
 		
 		System.out.println(":::::::Starting Server Console:::::::");
 		System.out.println(":::::::Type help for more info:::::::");
@@ -63,14 +138,17 @@ public class ServerConsole {
 	        if(message.equals("help")){
 	    		System.out.println(":::::::List of avalible commands:::::::");
 			 	System.out.println("create_account <login> <password> <user/staff/manager> <name> <e-mail> <phone_number> <commpany>");
-			 	System.out.println("create_room <name> <city> <location> <capacity> <rent_cost>");
+			 	System.out.println("create_room <owner_id> <name> <city> <location> <capacity> <rent_cost>");
 			 	System.out.println("list_accounts");
 			 	System.out.println("list_rooms");
-	        } else if(message.startsWith("create_account")){
+	        } else if(message.startsWith("create_account"))
 	        	newAccount(message);
-	        } else if(message.startsWith("list_accounts")){
-	        	list();
-	        }	        
+	        else if(message.startsWith("list_accounts"))
+	        	listAccounts();
+	        else if(message.startsWith("create_room"))
+	        	newRoom(message);
+	        else if(message.startsWith("list_rooms"))
+	        	listRooms();
 	      }
 	    } 
 	    catch (Exception ex) 
